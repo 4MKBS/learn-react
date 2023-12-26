504f915e8e455bbbe547199e6173055475cb409e
4mkbs <mkb4sakib@gmail.com>
2023-12-26 21:03:59 +0600

    functional component

--- src/main.jsx
+++ src/main.jsx
@@ -13,24 +13,22 @@ import './index.css'
 // let a=React.createElement('div',null,'hello world');
 
 // little bilding block of react is element
-
-
-setInterval(()=>{
-  let element = (
+function Clock({local}){
+  return(
     <div>
-      <h2>Hello World</h2>
+      <h2>Hello  World</h2>
       <h2>Hello React</h2>
       <div className="iammkbs"><h1>I am MKBS</h1></div>
       <img src="https://scontent.fjsr11-1.fna.fbcdn.net/v/t39.30808-6/361875275_1317758272426426_4987809083387574303_n.jpg?_nc_cat=101&ccb=1-7&_nc_sid=3635dc&_nc_ohc=XmnT_CgNrusAX-KJS2c&_nc_ht=scontent.fjsr11-1.fna&oh=00_AfCBs47org99VgdbTsTo-LLvQ4rqbOO-iTD7x8RjD7iWag&oe=658F23CF" alt="" height={400} width={400}   srcSet="" className='logo' />
-      <h1>{new Date().toLocaleTimeString()}</h1>
+      <h1>{new Date().toLocaleTimeString(local)}</h1>
     </div>
-  );//for multi line jsx we need to use () to wrap the jsx
-  // console.log(element);
-  // console.log(a.type);
-  // console.log(element.props.children[2].props);
-  
-  ReactDOM.createRoot(document.getElementById('root')).render(element)
-  
-  
+  );
 }
-,1000);  
+  ReactDOM.createRoot(document.getElementById('root')).render(
+    <React.StrictMode>
+      <Clock  local='bn-BD'/>,
+      <App />
+    </React.StrictMode>,
+  );
+  
+  
