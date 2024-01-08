c210c48fc6ebad72953ed68c49e8b72ec4700b13
4mkbs <mkb4sakib@gmail.com>
2024-01-08 13:51:17 +0600

    lifting state up and source of truth ,top down approch etc in react

--- src/components/Boiling.jsx
+++ src/components/Boiling.jsx
@@ -1,5 +1,8 @@
-export default function Boiling({celcius=0}){
-    if(celcius >= 100){
+export default function Boiling({celcius=0,scl}){
+    console.log(celcius);
+    if(celcius >= 100 && scl==='c'){
+        return <p>The water would boil.</p>
+    }else if(celcius >= 212 && scl==='f'){
         return <p>The water would boil.</p>
     }
     return <p>The water would not boil.</p>
--- src/components/Calculator.jsx
+++ src/components/Calculator.jsx
@@ -1,15 +1,28 @@
 import React from "react";
 import Boiling from "./Boiling";
 import TemperatureInput from "./TemperatureInput";
+import { convert, toCelsius, toFahrenheit } from "../lib/converter";
 
 export default class Calculator extends React.Component {
-   
+    state = {
+        temperature: '',
+        scale: 'c',
+    };
+   handler=(e,scale)=>{
+         this.setState({
+              temperature: e.target.value,
+              scale,
+         });
+   };
     render() {
+        const {temperature,scale}=this.state;
+        const celsius=scale==='f'?convert(temperature,toCelsius):temperature;
+        const fahrenheit=scale==='c'?convert(temperature,toFahrenheit):temperature;
         return (
             <>
-            <TemperatureInput scale="c" />
-            <TemperatureInput scale="f" />
-            {/* <Boiling celcius={parseFloat(temperature)} /> */}
+            <TemperatureInput scale="c" temperature={celsius} changeHandler={this.handler} />
+            <TemperatureInput scale="f" temperature={fahrenheit} changeHandler={this.handler} />
+            <Boiling celcius={parseFloat(temperature)} scl={scale} />
             </>
         )
     }
